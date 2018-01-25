c4405ad72df238462c14cf32c4d1b0d15305fd31
Vlad Alexandru <alexandru.vlad@ab4.systems>
2018-01-25 10:02:58 +0200

    Change new box computing

--- test.m
+++ test.m
@@ -24,6 +24,10 @@ boxes = [s.BoundingBox];
 boxes = vec2mat(boxes,4);
 nrBoxes = size(boxes,1);
 
+J = insertObjectAnnotation(rgbImage,'rectangle',boxes,boxes(:,1,1,1),'LineWidth',3, 'Color','red' ,'TextColor','black');
+figure
+imshow(J)
+
 done = 0;
 if nrBoxes == 1
     done = 1;
@@ -35,14 +39,22 @@ while done == 0
         for j=i+1 : nrBoxes
             overlapRatio = bboxOverlapRatio(boxes(i,:),boxes(j,:));
             if (overlapRatio ~= 0)
-                minX = min(boxes(i,1),boxes(j,1));
-                minY = min(boxes(i,2),boxes(j,2));
-                w = boxes(i,3) - boxes(j,1) + boxes(i,1);
-                w = max([w,boxes(i,3),boxes(j,3)]);
-                h = boxes(i,4) - boxes(j,2) + boxes(i,2);
-                h = max([h,boxes(i,4),boxes(j,4)]);
+                x1 = boxes(i,1);
+                x2 = boxes(j,1);
+                y1 = boxes(i,2);
+                y2 = boxes(j,2);
+                w1 = boxes(i,3);
+                w2 = boxes(j,3);
+                h1 = boxes(i,4);
+                h2 = boxes(j,4);
+                
+                minX = min([x1,x1+w1,x2,x2+w2]);
+                minY = min([y1,y1+h1,y2,y2+h2]);
+                maxX = max([x1,x1+w1,x2,x2+w2]);
+                maxY = max([y1,y1+h1,y2,y2+h2]);
+                
                 boxes([i,j],:) = [];
-                boxes = [boxes;minX,minY,w,h];
+                boxes = [boxes;[minX,minY,maxX-minX,maxY-minY]];
                 done = 0;
                 break
             end
